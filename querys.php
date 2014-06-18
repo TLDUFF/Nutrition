14d0b8eb557c18473dc3e426c858ce37c2bfca24
TLDUFF <TLDUFF@users.noreply.github.com>
2014-06-18 13:34:44 -0700

    Update querys.php
    Latest working copy for April and May 2014 reports.  Added ORDER BY upload date on new records and one recall completed.

--- querys.php
+++ querys.php
@@ -1,6 +1,6 @@
 <?php
 
- require_once('./Database.php');
+ require_once('Database.php');
 
             $db = new Database();
             $db->Connect();
@@ -18,7 +18,8 @@
             WHERE s.study = '$project'
               AND s.subjectID = v.subjectID
               AND s.printed = 'N'
-              AND s.flagged = 'N'";
+              AND s.flagged = 'N'
+            ORDER BY v.import_timestamp";
 
             $result_subjects=$db->Query($sql_subjects);
 
@@ -45,7 +46,7 @@
               AND s.subjectID = v.subjectID
               and s.printed = 'Y'";
 
-            $result_printed=$db->Query($sql_printed);
+        $result_printed=$db->Query($sql_printed);
 
     // INCOMPLETE INTERVIEWS
             $sql_incentives = "SELECT s.subjectID, s.first_name, s.last_name, s.womans_condition,
@@ -56,7 +57,31 @@
                  intake_from_food_raw r
             WHERE s.study = '$project'
               AND s.subjectID = r.externalstudyID
-            GROUP BY s.subjectID, s.first_name, s.last_name, s.womans_condition, AGE";
+            GROUP BY s.subjectID, s.first_name, s.last_name, s.womans_condition, AGE
+            ORDER BY r.dateofinterview";
 
             $result_incentives=$db->Query($sql_incentives);
+
+   //COUNT INTERVIEWS completed for subject.
+
+   function countInterviews($subjectID) {
+
+            $StudyID = $subjectID;
+
+            $db = new Database();
+            $db->Connect();
+
+            $sql_interviews="SELECT
+                count(dateofinterview) AS count
+             FROM intake_from_food_raw
+             WHERE externalstudyid = '$StudyID'
+             limit 1";
+
+            $interview_count = mysql_query($sql_interviews) or die();
+            $row = mysql_fetch_array($interview_count);
+            $count = $row[0];
+
+            return $count;
+
+   }
 ?>
