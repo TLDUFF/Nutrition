3533713a74807b7b53e59c1ed64b6c98903162e2
TLDUFF <TLDUFF@users.noreply.github.com>
2014-06-18 13:42:22 -0700

    Update subjectList.php
    Working version as of May 13. Added check for session_start();

--- subjectList.php
+++ subjectList.php
@@ -4,52 +4,60 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <link rel="stylesheet" type="text/css" href="accordionContent.css" />
         <script src="accordion.js"></script>
- 
-        
-    <?php   
-    // Display correct header for Project
-    if(isset($_POST['formProject']))
-    {
-        $project = $_POST['formProject'];
-    }// end if
 
-    switch ($project) 
-    {  
+    <?php
+    if(session_id() == '') {
+        session_start();
+    }
+
+    ?>
+
+        <div id="logout">
+            Welcome, <?php echo "{$_SESSION["myusername"]}"; ?>
+            <form name="logout" method="post" action="logout.php">
+                <input type="submit" name="Logout" value="Logout">
+            </form>
+        </div>
+
+    <?php
+    // Display correct header for Project
+    switch ($_SESSION['myproject'])
+    {
 
         case "OTIS" ?>
-            <IMG src="logoOTIS_RGB-300x173.jpg" 
-                 alt="Mother to Baby" 
+            <IMG src="logoOTIS_RGB-300x173.jpg"
+                 alt="Mother to Baby"
                  style="width: 150px; float: left; margin-left: 10px; margin-bottom: 8px; margin-right: 50px" />
             <?php break;
         case "RSG" ?>
-            <IMG src="logoRSG-outline.png" 
-                 alt="Ready, Set, Go" 
+            <IMG src="logoRSG-outline.png"
+                 alt="Ready, Set, Go"
                  style="width: 150px; float: left; margin-left: 10px; margin-bottom: 8px; margin-right: 50px" />
             <?php break;
         case "CTIS" ?>
-             <IMG src="logoCTIS.jpg" 
-                  alt="Mother to Baby California" 
+             <IMG src="logoCTIS.jpg"
+                  alt="Mother to Baby California"
                   style="width: 150px; float: left; margin-left: 10px; margin-bottom: 8px; margin-right: 50px" />
              <?php break;
 
     }//end switch
     ?>
-         
+
         <br />
-         <h2>List of Nutrition Assessment Participants from <?php echo "$project" ?> </h2>
+         <h2>List of Nutrition Assessment Participants from <?php echo "{$_SESSION['myproject']}" ?> </h2>
          <br /><br />
     </head>
 <!-- END HEADER -------------------------------------------------------------------------------------------------->
 
     <body>
-        
+
         <?php
-            require_once('querys.php');
+            require_once('./querys.php');
 
             /**Subject List output here (in Tables).**/
 
-            require_once('subjects.php'); 
-            
+            require_once('./subjects.php');
+
         ?>
 
 <br />
@@ -59,5 +67,6 @@
     <footer>
          <br /><br />
     </footer>
-    
+
 </html>
+~
